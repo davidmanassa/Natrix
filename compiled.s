7ca497904860a77421ad4f81f1de74fb1bcfe5de
David Pires <manassa97@hotmail.com>
2020-01-11 00:08:21 +0000

    ---

--- compiled.s
+++ compiled.s
@@ -2,16 +2,7 @@
 	.globl	main
 main:
 	movq %rsp, %rbp
-	movq $6, %rax
 	pushq %rax
-	movq $2, %rax
-	pushq %rax
-	popq %rbx
-	popq %rax
-	imulq %rbx, %rax
-	pushq %rax
-	popq %rdi
-	call print_int
 	movq $0, %rax
 	ret
 print_int:
